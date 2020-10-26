3fdf803d37afb94b49b9fea9a7a827181cab37f3
eisuke <eisuk4@gmail.com>
2020-10-26 18:24:15 +0900

    headタグ作成。phpディレクトリに共通化できる部品を格納していく。

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-phpinfo()
 ?>
+<html>
+  <?php include './php/head.php' ?>
+  <body>
+    <div class="hero is-primary">
+      <div class="hero-body">
+        <p class="title">Title</p>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ php/head.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title></title>
+    <meta name="format-detection" content="telephone=no">
+    <link rel="stylesheet" type="text/css" href="/stylesheet/bulma.min.css">
+</head>
