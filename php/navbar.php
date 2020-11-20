f5cf11c4852a19605e21456047532054b131415c
eisuke <eisuk4@gmail.com>
2020-11-20 11:16:15 +0900

    新規投稿のモーダルに文字数チェッカーを追加

--- php/navbar.php
+++ php/navbar.php
@@ -1,4 +1,4 @@
-<nav class='navbar'>
+<nav class='navbar' id="navbar">
     <div class='navbar-brand'>
     <div class=' navbar-item is-hidden-desktop'>
         <p>menu</p>
@@ -8,9 +8,52 @@
     <div class='navbar-end is-hidden-touch'>
     <div class='navbar-item'>
         <div class='buttons'>
-        <a href="" class='button is-primary'>Button1</a>
-        <a href="" class='button is-light'>新規投稿</a>
+        <a class='button is-light' @click="clickNewPost">新規投稿</a>
+        <div :class='[newPostModal === true ? "modal" : "modal is-active"]'>
+            <div class="modal-background" @click="clickNewPost"></div>
+            <div class="modal-card">
+                <div class="modal-card-head">
+                    <p>新規投稿</p>
+                </div>
+                <div class="modal-card-body">
+                    <textarea v-model.trim="newText" class="textarea" name="body" id=""></textarea>
+                    <p>{{newText.length}}/140</p>
+                </div>
+                <div class="modal-card-foot">
+                    <button class="button" @click="clickNewPost">Cancel</button>
+                    <input type="submit" class='button is-info' value="投稿する" v-bind:disabled="!cantSend"></input>
+                </div>
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="clickNewPost"></button>
+        </div>
         </div>
     </div>
     </div>
 </nav>
+
+<script>
+new Vue({
+    el: '#navbar',
+    data: {
+        newPostModal: true,
+        newText: '',
+        cantSend: false
+    },
+    methods: {
+        clickNewPost: function () {
+            this.newPostModal = !this.newPostModal
+        }
+    },
+    watch: {
+        newText: function () {
+            console.log(this.newText.length)
+            if (this.newText.length > 140 || this.newText.length <= 0) {
+                this.cantSend = false
+            }
+            if (this.newText.length > 0 && this.newText.length <= 140) {
+                this.cantSend = true
+            }
+        }
+    }
+})
+</script>
