221fe4f9cf5bc7fbfd5ffe2d5c5da9c8b8c88662
eisuke <eisuke@localhost.localdomain>
2020-12-03 23:23:03 +0900

    フッターを修正

--- php/footer.php
+++ php/footer.php
@@ -1,7 +1,7 @@
 <footer class='footer'>
     <div class='content has-text-centered'>
         <p>
-            <strong>Title</strong> by eisuke.
+            <strong>Ordoozm</strong> by eisuke.
         </p>
     </div>
 </footer>
